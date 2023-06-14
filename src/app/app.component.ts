9e772046507525b73ebe9a3deaf525084af06b3f
rroldan <roldan.ricardo@gmail.com>
2023-06-14 23:07:57 +0200

    start angular-wsam example

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { add } from 'wasm-lib/pkg/wasm_lib_bg.wasm'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'angular-rust-wasm';
+  total = 0;
+
+  ngOnInit() {
+    this.calc();
+  }
+
+  async calc() {
+    this.total = add(1, 1);
+    }
+}
