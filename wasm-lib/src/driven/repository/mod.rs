f7981f6f9a453bf08db5d663dcd3d3490083bb32
rroldan <roldan.ricardo@gmail.com>
2023-07-02 23:59:35 +0200

    add repository

--- wasm-lib/src/driven/repository/mod.rs
+++ wasm-lib/src/driven/repository/mod.rs
@@ -1,6 +1,52 @@
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FindTipoVivienda {
+    pub id: Option<String>,
+    pub calle: Option<String>,
+    pub numero: i32,
+    pub piso: String,
+    pub codigo_postal: String,
+    pub metros_cuadrados: i32,
+    pub numero_aseos: i32,
+    pub numero_habitaciones: i32,
+    pub tipo: Tipo
+}
+
+#[derive(Debug)]
+pub enum RepoCreateError {
+    InvalidData(String),
+    Unknown(String)
+}
+
+#[derive(Debug)]
+pub enum RepoSelectError {
+    NotFound,
+    Unknown(String)
+}
+
+#[derive(Debug)]
+pub enum RepoFindAllError {
+    Unknown(String)
+}
+
+#[derive(Debug)]
+pub enum RepoUpdateError {
+    InvalidData(String),
+    NotFound,
+    Unknown(String)
+}
+
+#[derive(Debug)]
+pub enum RepoDeleteError {
+    NotFound,
+    InvalidData(String),
+    Unknown(String)
+}
+
+#[async_trait]
 pub trait Repository<T> where T: Entity {
 
     /// A function responsible for the creation of the Repository
